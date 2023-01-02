94425a9dfa190bb9a7b29b84150f64705af13409
Barkın Bayzin <bayesiian@gmail.com>
2023-01-02 14:29:49 +0300

    Add project files.

---
+++ AsynchronousProgramming/Models/AutoMapper/Mapping.cs
@@ -0,0 +1,17 @@
+﻿using AsynchronousProgramming.Models.DTOs;
+using AsynchronousProgramming.Models.Entities.Concrete;
+using AsynchronousProgramming.Models.VMs;
+using AutoMapper;
+
+namespace AsynchronousProgramming.Models.AutoMapper
+{
+    public class Mapping : Profile
+    {
+        public Mapping()
+        {
+            CreateMap<Category, CreateCategoryDTO>().ReverseMap();
+            CreateMap<Category, UpdateCategoryDTO>().ReverseMap();
+            CreateMap<Category, CategoryVM>().ReverseMap();
+        }
+    }
+}
