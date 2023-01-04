4b79cd20709a42837579989ee33bd993f2e60b3c
Barkın Bayzin <bayesiian@gmail.com>
2023-01-04 12:37:42 +0300

    Product Create and List added..

--- AsynchronousProgramming/Controllers/ProductController.cs
+++ AsynchronousProgramming/Controllers/ProductController.cs
@@ -1,8 +1,17 @@
 ﻿using AsynchronousProgramming.Infrastructure.Repositories.Interfaces;
+using AsynchronousProgramming.Models.DTOs;
+using AsynchronousProgramming.Models.Entities.Abstract;
 using AsynchronousProgramming.Models.Entities.Concrete;
+using AsynchronousProgramming.Models.VMs;
 using AutoMapper;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace AsynchronousProgramming.Controllers
 {
@@ -21,9 +30,66 @@ namespace AsynchronousProgramming.Controllers
             _webHostEnvironment = webHostEnvironment;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Create()
         {
+            ViewBag.Categories = new SelectList(await _catRepository.GetByDefaults(x => x.Status != Status.Passive), "Id", "Name");
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                string imageName = "noimage.png";
+                if(model.UploadImage != null)
+                {
+                    //Dosyanın kök dizindeki yerini yakalıyorum
+                    string uploadDir = Path.Combine(_webHostEnvironment.WebRootPath, "media/products");
+
+                    //Yüklenecek dosyaların aynı dosya olsa bile birbirleri ile çakışmaması için uniqueleştirmeye çalışıyorum burada kullandığımız temp. (Guid_FileName)
+                    imageName = $"{Guid.NewGuid()}_{model.UploadImage.FileName}";
+
+                    //Artık elimde yüklenecek dizin ve dosya mevcut, benim bunları fileStream sınıfına göndermek için combine ediyorum
+                    string filePath = Path.Combine(uploadDir, imageName);
+
+                    //Yaratılan dosya yolunun kayıt işlemini başlatmak için instance alınıfrken ctor'a parametrelerimi iletiyorum.
+                    FileStream fileStream = new FileStream(filePath, FileMode.Create);
+
+                    //ve dosyayı yazma/ekleme/kopyalama işlemi bu satırda tamamlanıyor
+                    await model.UploadImage.CopyToAsync(fileStream);
+                    //stream'i kapatmazsak hata alırız
+                    fileStream.Close(); 
+                }
+
+                Product product = _mapper.Map<Product>(model);
+                product.Image = imageName;
+                await _proRepository.Add(product);
+                TempData["Success"] = "The product has been created..!";
+                return RedirectToAction("List");
+            }
+            TempData["Error"] = "The product hasn't been created..!";
+            return View();
+        }
+
+        public async Task<IActionResult> List()
+        {
+            var products = await _proRepository.GetFilteredList(
+                select: x => new ProductVM
+                {
+                    Id = x.Id,
+                    Name = x.Name,
+                    CategoryName = x.Category.Name,
+                    Image = x.Image,
+                    UnitPrice = x.UnitPrice,
+                    Status = x.Status,
+                    Description = x.Description
+                },
+                where: x => x.Status != Status.Passive,
+                orderBy: x => x.OrderByDescending(x => x.CreateDate),
+                join: x => x.Include(x => x.Category)); //eager loading
+
+            return View(products);
         }
     }
 }
